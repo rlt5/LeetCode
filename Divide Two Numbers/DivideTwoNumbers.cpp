aaa744604f1b768a81cd2f29b7fe33afd5769d94
Robin Truong <robintrg007@gmail.com>
2020-04-28 04:49:52 -0700

    Fixed all bugs. LeetCode submission successful. Need to clean up code.

--- Divide Two Numbers/DivideTwoNumbers.cpp
+++ Divide Two Numbers/DivideTwoNumbers.cpp
@@ -8,8 +8,8 @@ class Solution {
 public:
 
     //function for Binary Addition
-    int binAddition(int a,int b){
-        int c; //carry
+    u_int32_t binAddition(u_int32_t a,u_int32_t b){
+        u_int32_t c; //carry
         while (b != 0) {
             //find carry and shift it left
             c = (a & b) << 1;
@@ -21,8 +21,8 @@ public:
     }
 
     //function for Binary Subtraction
-    int binSubtraction(int a, int b){
-        int carry;
+    u_int32_t binSubtraction(u_int32_t a, u_int32_t b){
+        u_int32_t carry;
         //get 2's compliment of b and add in a
         b = binAddition(~b, 1); 
         while (b != 0) {
@@ -36,32 +36,42 @@ public:
     }
 
     int divide(int dividend2, int divisor) {
-        u_int32_t dividend = dividend2;
+        u_int32_t dividend = (u_int32_t)dividend2;
+        u_int32_t divisor2 = (u_int32_t)divisor;
         if ( divisor == 0 ) {
             std::cout << "ERROR: Divide By Zero" << std::endl;
             return 0;
 
         }
-        if ( dividend == -2147483648 && divisor == -1 ) return 2147483647;
+        if ( dividend2 == -2147483648 && divisor == -1 ) return 2147483647;
+        if ( dividend2 == -2147483648 && divisor == 1 ) return -2147483648;
 
-        int diff;
-        int sum = 0, result = 0;
+        // int diff;
+        // int sum = 0, result = 0;
         u_int32_t quotient = 0, remainder = 0;
-        for(int i = 31 ; i >= 0 ; i--)
-        {
-            quotient <<= 1;
-            remainder <<= 1;
-            remainder |= (dividend & (1 << i)) >> i;
+        // for(int i = 31 ; i >= 0 ; i--)
+        // {
+        //     quotient <<= 1;
+        //     remainder <<= 1;
+        //     remainder |= (dividend & (1 << i)) >> i;
 
-            if(remainder >= divisor)
-            {
-                remainder = binSubtraction(remainder,divisor);
-                quotient |= 1;
-            }
-            // std::cout << "quotient = " << quotient << std::endl;
-        }
+        //     if(remainder >= divisor2)
+        //     {
+        //         remainder = binSubtraction(remainder,divisor2);
+        //         quotient |= 1;
+        //     }
+        //     // std::cout << "quotient = " << quotient << std::endl;
+        // }
         
         if ( 0 < dividend2  && divisor < 0 ){
+            if ( divisor == -2147483648 ){
+                divisor2 = 2147483648;
+            } else {
+                divisor2 = (u_int32_t)-divisor;
+            }
+            
+            // std::cout << "dividend = " << dividend << std::endl;
+            // std::cout << "divisor2 = " << divisor2 << std::endl;
             // diff = INT_MIN - divisor;
             // while ( sum >= -dividend2 ){
             //     result++;
@@ -72,8 +82,33 @@ public:
             //     sum += divisor;
             // }            
             // return -result+1;
-            return (int)quotient;
+            for(int i = 31 ; i >= 0 ; i--)
+            {
+                quotient <<= 1;
+                remainder <<= 1;
+                remainder |= (dividend & (1 << i)) >> i;
+
+                if(remainder >= divisor2)
+                {
+                    remainder = binSubtraction(remainder,divisor2);
+                    quotient |= 1;
+                }
+                // std::cout << "quotient = " << quotient << std::endl;
+            }
+
+
+            return (int)-quotient;
         } else if ( dividend2 < 0 && divisor < 0 ) {
+            if ( dividend2 == -2147483648 ){
+                dividend = 2147483648;
+            } else {
+                dividend = (u_int32_t)-dividend2;
+            }
+            if ( divisor == -2147483648 ){
+                divisor2 = 2147483648;
+            } else {
+                divisor2 = (u_int32_t)-divisor;
+            }
             // diff = INT_MAX + divisor;
             // sum = dividend;
             // while ( sum <= 0 ){
@@ -85,8 +120,28 @@ public:
             //     sum -= divisor;
             // }  
             // return result-1;
-            return -quotient;
+            // 
+            for(int i = 31 ; i >= 0 ; i--)
+            {
+                quotient <<= 1;
+                remainder <<= 1;
+                remainder |= (dividend & (1 << i)) >> i;
+
+                if(remainder >= divisor2)
+                {
+                    remainder = binSubtraction(remainder,divisor2);
+                    quotient |= 1;
+                }
+                // std::cout << "quotient = " << quotient << std::endl;
+            }
+            return quotient;
         } else if ( dividend2 < 0 &&  0 < divisor ){
+            
+            if ( dividend2 == -2147483648 ){
+                dividend = 2147483648;
+            } else {
+                dividend = (u_int32_t)-dividend2;
+            }
             // diff = INT_MIN + divisor;
             // while ( sum >= dividend ){
             //     if ( sum < diff ){
@@ -97,19 +152,19 @@ public:
             // } 
             // return -result+1;
 
-            // for(int i = 31 ; i >= 0 ; i--)
-            // {
-            //     quotient <<= 1;
-            //     remainder <<= 1;
-            //     remainder |= (dividend & (1 << i)) >> i;
+            for(int i = 31 ; i >= 0 ; i--)
+            {
+                quotient <<= 1;
+                remainder <<= 1;
+                remainder |= (dividend & (1 << i)) >> i;
 
-            //     if(remainder >= divisor)
-            //     {
-            //         remainder = binSubtraction(remainder,divisor);
-            //         quotient |= 1;
-            //     }
-            //     // std::cout << "quotient = " << quotient << std::endl;
-            // }
+                if(remainder >= divisor2)
+                {
+                    remainder = binSubtraction(remainder,divisor2);
+                    quotient |= 1;
+                }
+                // std::cout << "quotient = " << quotient << std::endl;
+            }
 
             return -quotient;
             // diff = INT_MIN + divisor;
@@ -130,19 +185,22 @@ public:
             // }
             // return result - 1;
 
+            // std::cout << "dividend = " << dividend << std::endl;
+            // std::cout << "divisor2 = " << divisor2 << std::endl;
 
-            // for(int i = 31 ; i >= 0 ; i--){
-            //     quotient <<= 1;
-            //     remainder <<= 1;
-            //     remainder |= (dividend & (1 << i)) >> i;
+            for(int i = 31 ; i >= 0 ; i--){
+                quotient <<= 1;
+                remainder <<= 1;
+                remainder |= (dividend & (1 << i)) >> i;
 
-            //     if(remainder >= divisor)
-            //     {
-            //         remainder = binSubtraction(remainder,divisor);
-            //         quotient |= 1;
-            //     }
-            //     // std::cout << "quotient = " << quotient << std::endl;
-            // }
+                if(remainder >= divisor2)
+                {
+                    remainder = binSubtraction(remainder,divisor2);
+                    quotient |= 1;
+                }
+                // std::cout << "quotient = " << quotient << std::endl;
+                // std::cout << "remainder = " << remainder << std::endl;
+            }
             return quotient;
         }
     }
@@ -157,6 +215,10 @@ int main(){
     list.push_back({10,3});
     list.push_back({7,-3});
     list.push_back({-2147483648,1});
+    list.push_back({-2147483648,2});
+    list.push_back({-1,1});
+    list.push_back({-1,-1});
+    list.push_back({1040040164,163207087});
 
     
     int dividend;
@@ -179,11 +241,12 @@ int main(){
         }
     }
 
-    int y = -2147483648;
-    for (int i = 0; i < 10; i++ ){
-        y = y >> i;
-        std::cout << i << ": y = " << y << std::endl;
-    }
+    // int y = -1;
+    // std::cout <<"y = " << (u_int32_t)y-4294967294 << std::endl;
+    // for (int i = 0; i < 10; i++ ){
+    //     y = y >> i;
+    //     std::cout << i << ": y = " << y << std::endl;
+    // }
 
     // int result = sol->divide(dividend,divisor);
     // if ( sol->divide(dividend,divisor) != dividend/divisor ){
