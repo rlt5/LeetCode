f72027784176af53be1278f4edc52f8bf94e8881
Robin Truong <robintrg007@gmail.com>
2020-04-30 03:51:47 -0700

    O(n) Stack solution

--- Longest Valid Parentheses/LongestValidParentheses.cpp
+++ Longest Valid Parentheses/LongestValidParentheses.cpp
@@ -2,97 +2,33 @@
 // #include <stack>
 #include <bits/stdc++.h> 
 #include <string>
+#include <stack>
 #include <list>
 
 using namespace std;
 
+////////////////////////////////////////////////////////////////
+//// BRUTE FORCE O(n^3)
 // bool validParentheses(string s){
-//     stack<char>* stk = new stack<char>();
+//     int count = 0;
 //     for (char c : s){
 //         if ( c == '('){
-//             stk->push(c);
+//             count++;
 //         } else {
-//             if (!stk->empty()){
-//                 stk->pop();
+//             if (count){
+//                 count--;
 //             } else {
 //                 return false;
 //             }
 //         }
 //     }
-//     if ( stk->empty() ){
+//     if ( count == 0 ){
 //         return true;
 //     } else 
 //         return false;
 // }
-
-bool validParentheses(string s){
-    int count = 0;
-    for (char c : s){
-        if ( c == '('){
-            count++;
-        } else {
-            if (count){
-                count--;
-            } else {
-                return false;
-            }
-        }
-    }
-    if ( count == 0 ){
-        return true;
-    } else 
-        return false;
-}
-
-int longestValidParentheses(string s){
-    int length = s.length();
-    if (length == 0) return 0; 
-    // int maxLength = 0;
-    int maxLength[length];
-    for ( int i = 0; i < length; i++ ){
-        maxLength[i] = 0;
-    }  
-
-    int count = 0, currentMax = 0, countL = 0;
-    for ( int i = 0; i < length; i++ ){
-        currentMax = 0;
-        count = 0;
-        for ( int j = i; j < length; j++){
-
-            if ( s[j] == '('){
-                count++;
-            } else {
-                if ( count ){
-                    count--;
-                    currentMax++;
-                } else {
-                    currentMax = 0;
-                }
-            }
-
-            if ( count == 0 ){
-                if (maxLength[j] < currentMax ){
-                    maxLength[j] = currentMax;
-                }
-            }
-        }
-    }
-
-    int maxReturn = 0;
-    for ( int i = 0; i < length; i++ ){
-        // cout << maxLength[i] << ",";
-        if ( maxReturn < maxLength[i] ){
-            maxReturn = maxLength[i];
-        }
-    }
-    // cout << "---";
-
-    return maxReturn*2;
-
-}
-
-
-//// OLD CODE THAT IS N^3.
+// 
+// 
 // int longestValidParentheses(string s){
 //     int length = s.length();
 //     // int maxLength[length];
@@ -111,6 +47,103 @@ int longestValidParentheses(string s){
 //     }
 //     return maxLength;
 // }
+////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////
+// O(n^2) dp solution
+// int longestValidParentheses(string s){
+//     int length = s.length();
+//     if (length == 0) return 0; 
+
+//     int maxLength[length];
+//     for ( int i = 0; i < length; i++ ){
+//         maxLength[i] = 0;
+//     }  
+
+//     int count = 0, currentMax = 0, countL = 0;
+//     for ( int i = 0; i < length; i++ ){
+//         currentMax = 0;
+//         count = 0;
+//         for ( int j = i; j < length; j++){
+
+//             if ( s[j] == '('){
+//                 count++;
+//             } else {
+//                 if ( count ){
+//                     count--;
+//                     currentMax++;
+//                 } else {
+//                     currentMax = 0;
+//                 }
+//             }
+// 
+//             if ( count == 0 ){
+//                 if (maxLength[j] < currentMax ){
+//                     maxLength[j] = currentMax;
+//                 }
+//             }
+//         }
+//     }
+// 
+//     int maxReturn = 0;
+//     for ( int i = 0; i < length; i++ ){
+//         if ( maxReturn < maxLength[i] ){
+//             maxReturn = maxLength[i];
+//         }
+//     }
+//     return maxReturn*2;
+// }
+////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////
+// O(n) dp solution
+// int longestValidParentheses(string s){
+//     int length = s.length();
+//     if (length == 0) return 0; 
+//     int maxLength[length];
+//     int maxReturn = 0;
+//     for ( int i = 0; i < length; i++ ){
+//         maxLength[i] = 0;
+//     }  
+//     for ( int i = 1; i < length; i++ ){
+//         if ( s[i] == ')' ){
+//             if ( s[i-1] == '(' ){
+//                 maxLength[i] = (i >= 2 ? maxLength[i-2] : 0) + 2;
+//             } else if ( i - maxLength[i-1] > 0 && s[i-maxLength[i-1]-1] == '(' ){
+//                 maxLength[i] = maxLength[i - 1] + ((i - maxLength[i - 1]) >= 2 ? maxLength[i - maxLength[i - 1] - 2] : 0) + 2;
+//             }
+//             if ( maxReturn < maxLength[i] )
+//                 maxReturn = maxLength[i];
+//         }
+//     }
+//     return maxReturn;
+// }
+////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////
+// O(n) STACK Solution
+int longestValidParentheses(string s) {
+    int maxReturn = 0;
+    stack<int> stack;
+    stack.push(-1);
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == '(') {
+            stack.push(i);
+        } else {
+            stack.pop();
+            if (stack.empty()) {
+                stack.push(i);
+            } else {
+                maxReturn = max(maxReturn, i - stack.top());
+            }
+        }
+    }
+    return maxReturn;
+}
+////////////////////////////////////////////////////////////////
+
+
 
 int main(){
 
