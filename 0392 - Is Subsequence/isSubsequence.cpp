23c93ff94625763155f661af8486904ec61b15b7
Robin Truong <robintrg007@gmail.com>
2020-06-09 00:23:42 -0700

    Complete June 9, 2020 Challenge

---
+++ 0392 - Is Subsequence/isSubsequence.cpp
@@ -0,0 +1,59 @@
+// 392. Is Subsequence
+// Given a string s and a string t, check if s is subsequence of t.
+// A subsequence of a string is a new string which is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (ie, "ace" is a subsequence of "abcde" while "aec" is not).
+
+// Follow up:
+// If there are lots of incoming S, say S1, S2, ... , Sk where k >= 1B, and you want to check one by one to see if T has its subsequence. In this scenario, how would you change your code?
+
+// Credits:
+// Special thanks to @pbrother for adding this problem and creating all test cases.
+
+ 
+
+// Example 1:
+
+// Input: s = "abc", t = "ahbgdc"
+// Output: true
+// Example 2:
+
+// Input: s = "axc", t = "ahbgdc"
+// Output: false
+ 
+
+// Constraints:
+
+// 0 <= s.length <= 100
+// 0 <= t.length <= 10^4
+// Both strings consists only of lowercase characters.
+
+// Test Cases
+// "abc"
+// "ahbgdc"
+// "axc"
+// "ahbgdc"
+// "how"
+// "helloworld"
+// ""
+// "abc"
+// "abc"
+// "a"
+
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int currentS = 0;
+        int currentT = 0;
+        while ( currentS < s.size() && currentT < t.size()  ){
+            if ( s[currentS] == t[currentT] ){
+                currentS++;
+            }
+            currentT++;
+        }
+        if ( currentS == s.size() )
+            return true;
+        return false;
+    }
+};
